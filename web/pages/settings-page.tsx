1739430a5a40613b9bd0a73b043543b26b63e1bf
Denny Li <ytdli08@gmail.com>
2025-07-29 23:28:08 -0400

    updated settings page with prompt parameter

--- web/pages/settings-page.tsx
+++ web/pages/settings-page.tsx
@@ -4,12 +4,28 @@ const SettingsPage: React.FC = () => {
   const [copy, setCopy] = useState(true);
   useEffect(() => {
     setCopy(localStorage.getItem("copy") === "true");
+    const quickSuggestions = localStorage.getItem("quickSuggestions");
+    if (!quickSuggestions) localStorage.setItem("quickSuggestions", "true");
   }, []);
   return (
     <div className="w-full">
       <div className="flex items-center gap-3 justify-center">
         <img src={box} alt="logo icon" className="h-16 dark:invert" />
         <p className="text-2xl font-medium ">Settings</p>
+      </div>
+      <div className="w-full flex text-md mt-2 p-4 justify-center border-2 rounded border-gray-300 text-gray-600 dark:border-gray-600 dark:text-gray-400">
+        <legend>Quick Suggestions: </legend>
+        <select
+          className="bg-[var(--background)]"
+          defaultValue={localStorage.getItem("quickSuggestions") || "true"}
+          onChange={(e) => {
+            localStorage.setItem("quickSuggestions", e.target.value);
+          }}
+          id="quick-suggestions-form"
+        >
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
       </div>
       <div className="w-full flex text-md mt-2 p-4 justify-center border-2 rounded border-gray-300 text-gray-600 dark:border-gray-600 dark:text-gray-400">
         <legend>Copy Prompt Instead: </legend>
@@ -26,8 +42,14 @@ const SettingsPage: React.FC = () => {
           <option value="false">No</option>
         </select>
       </div>
-      <div className={`${copy ? "hidden" : ""} w-full flex text-md mt-2 p-4 justify-center border-2 rounded border-gray-300 text-gray-600 dark:border-gray-600 dark:text-gray-400`}>
-        <legend>Use prompt parameter: </legend>
+      <div
+        className={`${
+          copy ? "hidden" : ""
+        } w-full flex text-md mt-2 p-4 justify-center border-2 rounded border-gray-300 text-gray-600 dark:border-gray-600 dark:text-gray-400`}
+      >
+        <legend>
+          Use Parameter <code className="font-mono">prompt?=%s</code>:
+        </legend>
         <select
           className="bg-[var(--background)]"
           defaultValue={localStorage.getItem("prompt") || "false"}
